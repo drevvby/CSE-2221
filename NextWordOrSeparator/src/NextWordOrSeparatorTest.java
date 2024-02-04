e1e25465a08194f2d3533a61ee5792f87fa83767
Andrew White <157429578+drevvby@users.noreply.github.com>
2024-02-04 16:08:20 -0500

    Added CSE 2221 project to archive
    Java program that determines if a word from a given line of text input is a word or a separator defined by a predetermined set of separator characters such as commas and spaces.

---
+++ NextWordOrSeparator/src/NextWordOrSeparatorTest.java
@@ -0,0 +1,167 @@
+import components.set.Set;
+import components.set.Set1L;
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+
+/**
+ * Program to test static methods {@code generateElements} and
+ * {@code nextWordOrSeparator}.
+ *
+ * @author Andrew White
+ *
+ */
+public final class NextWordOrSeparatorTest {
+
+    /**
+     * Private constructor so this utility class cannot be instantiated.
+     */
+    private NextWordOrSeparatorTest() {
+    }
+
+    /**
+     * Generates the set of characters in the given {@code String} into the
+     * given {@code Set}.
+     *
+     * @param str
+     *            the given {@code String}
+     * @param charSet
+     *            the {@code Set} to be replaced
+     * @replaces charSet
+     * @ensures charSet = entries(str)
+     */
+    private static void generateElements(String str, Set<Character> charSet) {
+        assert str != null : "Violation of: str is not null";
+        assert charSet != null : "Violation of: charSet is not null";
+
+        //initialize counter and character that will be replaced
+        int count = 0;
+
+        //while count is less than the input string str's length, c will be set to the
+        //current character of str and added to the charSet, creating a character set
+        //full of every unique character in str
+        while (count < str.length()) {
+            char c = str.charAt(count);
+            if (!charSet.contains(c)) {
+
+                charSet.add(c);
+            }
+            count++;
+        }
+
+    }
+
+    /**
+     * Returns the first "word" (maximal length string of characters not in
+     * {@code SEPARATORS}) or "separator string" (maximal length string of
+     * characters in {@code SEPARATORS}) in the given {@code text} starting at
+     * the given {@code position}.
+     *
+     * @param text
+     *            the {@code String} from which to get the word or separator
+     *            string
+     * @param position
+     *            the starting index
+     * @return the first word or separator string found in {@code text} starting
+     *         at index {@code position}
+     * @requires 0 <= position < |text|
+     * @ensures <pre>
+     * nextWordOrSeparator =
+     *   text[position, position + |nextWordOrSeparator|)  and
+     * if entries(text[position, position + 1)) intersection entries(SEPARATORS) = {}
+     * then
+     *   entries(nextWordOrSeparator) intersection entries(SEPARATORS) = {}  and
+     *   (position + |nextWordOrSeparator| = |text|  or
+     *    entries(text[position, position + |nextWordOrSeparator| + 1))
+     *      intersection entries(SEPARATORS) /= {})
+     * else
+     *   entries(nextWordOrSeparator) is subset of entries(SEPARATORS)  and
+     *   (position + |nextWordOrSeparator| = |text|  or
+     *    entries(text[position, position + |nextWordOrSeparator| + 1))
+     *      is not subset of entries(SEPARATORS))
+     * </pre>
+     */
+    private static String nextWordOrSeparator(Set<Character> separatorSet,
+            String text, int position) {
+        assert text != null : "Violation of: text is not null";
+        assert 0 <= position : "Violation of: 0 <= position";
+        assert position < text.length() : "Violation of: position < |text|";
+
+        int i = position;
+        while (i < text.length()
+                && separatorSet.contains(text.charAt(position)) == separatorSet
+                        .contains(text.charAt(i))) {
+            i++;
+        }
+        return text.substring(position, i);
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+         * Define separator characters for test
+         */
+        final String separatorStr1 = " \t, ";
+        final String separatorStr2 = ":";
+        final String separatorStr3 = ",";
+        final String separatorStr4 = ";";
+        Set<Character> separatorSet = new Set1L<>();
+        generateElements(separatorStr1, separatorSet);
+        generateElements(separatorStr2, separatorSet);
+        generateElements(separatorStr3, separatorSet);
+        generateElements(separatorStr4, separatorSet);
+        /*
+         * Open input and output streams
+         */
+        SimpleReader in = new SimpleReader1L();
+        SimpleWriter out = new SimpleWriter1L();
+        /*
+         * Ask for test cases
+         */
+        out.println();
+        out.print("New test case (y/n)? ");
+        String response = in.nextLine();
+        while (response.equals("y")) {
+            /*
+             * Output heading
+             */
+            out.print("Test case: ");
+            String testStr = in.nextLine();
+            out.println();
+            out.println("----Next test case----");
+            out.println();
+            /*
+             * Process test case
+             */
+            int position = 0;
+            while (position < testStr.length()) {
+                String token = nextWordOrSeparator(separatorSet, testStr,
+                        position);
+                if (separatorSet.contains(token.charAt(0))) {
+                    out.print("  Separator: <");
+                } else {
+                    out.print("  Word: <");
+                }
+                out.println(token + ">");
+                position += token.length();
+            }
+            /*
+             * Ask user whether to continue
+             */
+            out.println();
+            out.print("New test case (y/n)? ");
+            response = in.nextLine();
+        }
+        /*
+         * Close input and output streams
+         */
+        in.close();
+        out.close();
+    }
+}
